fbdf8f3d1ce59d8d1e388c65bf0c23fff1006a94
Karthik Ramanan <a0393906@ti.com>
2016-03-23 14:59:21 +0530

    cpuload-plugins: Cleanup scripts
    Signed-off-by: Karthik Ramanan <a0393906@ti.com>

--- cpuload-plugins/scripts/dummy_cpu_load.sh
+++ cpuload-plugins/scripts/dummy_cpu_load.sh
@@ -1,13 +1,5 @@
 delay=100000
-	echo "CPULOAD: MPU $((RANDOM%100))" > /tmp/socfifo
-	usleep $delay
-	echo "CPULOAD: IPU1 $((RANDOM%100))" > /tmp/socfifo
-	usleep $delay
-	echo "CPULOAD: IPU2 $((RANDOM%100))" > /tmp/socfifo
-	usleep $delay
-	echo "CPULOAD: DSP1 $((RANDOM%100))" > /tmp/socfifo
-	usleep $delay
-	echo "CPULOAD: DSP2 $((RANDOM%100))" > /tmp/socfifo
-	usleep $delay
-	echo "CPULOAD: GPU $((RANDOM%100))" > /tmp/socfifo
-	usleep $delay
+echo "CPULOAD: MPU $((RANDOM%100))" > /tmp/socfifo
+usleep $delay
+echo "CPULOAD: GPU $((RANDOM%100))" > /tmp/socfifo
+usleep $delay
