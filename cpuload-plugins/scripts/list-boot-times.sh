f2c08b3267f828bf2cc7d6cd85ddcd2d22ea3d74
Venkat Mandela <venkat.mandela@ti.com>
2016-11-10 21:03:43 +0000

    cpuload-plugins: boot time script improvements
    1. Fix shellcheck warnings.
    2. Print benchmark numbers in the order
       MLO,
       Kernel,
       Userspace
    3. Print kernel decompression time and kernel execution time
    
    Signed-off-by: Venkateswara Rao Mandela <venkat.mandela@ti.com>

--- cpuload-plugins/scripts/list-boot-times.sh
+++ cpuload-plugins/scripts/list-boot-times.sh
@@ -1,8 +1,18 @@
 #! /bin/bash 
-cd /tmp
-FILE_NAMES=$(ls -1 [kmu]-*)
-for FILE in $FILE_NAMES; do
-    FILE_CONTENT=$(cat $FILE)
-    echo $FILE,$FILE_CONTENT
+cd /tmp || exit
+DISPORDER="m k u"
+for DISP in $DISPORDER; do
+    FILE_NAMES=$(ls -1 "$DISP"-*)
+    for FILE in $FILE_NAMES; do
+        FILE_CONTENT=$(cat "$FILE")
+        echo "$FILE","$FILE_CONTENT"
+    done
+    echo
 done
 
+KERNEL_START=$(cat /tmp/m-kernelstart-time)
+KERNEL_MAIN=$(cat /tmp/k-start-time)
+echo "Kernel Decompression time",$(( KERNEL_MAIN - KERNEL_START))
+
+USER_ENTRY=$(cat /tmp/k-user-space-entry-time)
+echo "Kernel Exec time",$(( USER_ENTRY - KERNEL_START))
